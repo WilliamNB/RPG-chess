3d33e778178456728583f9f301d4475457bb8ff8
WilliamNB <59480886+WilliamNB@users.noreply.github.com>
2020-04-21 19:25:58 +0100

    Shows board

---
+++ Rpg Chess/Assets/Scripts/Board.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Board : MonoBehaviour
+{
+
+    public GameObject mCellPrefab;
+    [HideInInspector]
+    public Cell[,] mAllCells = new Cell[8, 8];
+
+    public void create()
+    {
+        for(int y = 0; y < 8; y++)
+        {
+            for(int x = 0; x < 8; x++)
+            {
+                //create the cell
+                GameObject newCell = Instantiate(mCellPrefab, transform);
+                //positon
+                RectTransform rectTransform = newCell.GetComponent<RectTransform>();
+                rectTransform.anchoredPosition = new Vector2((x * 100) + 50, (y * 100) + 50);
+                //setup
+                mAllCells[x, y] = newCell.GetComponent<Cell>();
+                mAllCells[x, y].setup(new Vector2Int(x, y), this);
+            }
+        }
+
+        //colour the board
+        for(int y = 0; y < 8; y++)
+        {
+            for(int x = 0; x < 8; x += 2)
+            {
+                int offset = (y % 2 != 0) ? 0 : 1;
+                int finalX = x + offset;
+
+                mAllCells[finalX, y].GetComponent<Image>().color = new Color32(230, 220, 187, 255);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Rpg Chess/Assets/Scripts/Cell.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Specialized;
+
+public class Cell : MonoBehaviour
+{
+
+    public Image mOutLineImage;
+    [HideInInspector]
+    public Vector2Int mBoardPos;
+    [HideInInspector]
+    public Board mBoard = null;
+    [HideInInspector]
+    public RectTransform mRectTransform = null;
+
+    public void setup(Vector2Int newBoardPos, Board newBoard)
+    {
+        mBoardPos = newBoardPos;
+        mBoard = newBoard;
+
+        mRectTransform = GetComponent<RectTransform>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
